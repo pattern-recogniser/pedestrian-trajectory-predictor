a03ede20dff29357e2608e7e79cc3d801f070d11
Anjali Karimpil <anjaliskarimpil@gmail.com>
2018-08-02 11:35:27 +0100

    created class for reading in data.

--- data_utils.py
+++ data_utils.py
@@ -1,14 +1,17 @@
 '''
 Author: Anjali Sebastian Karimpil
 '''
-import numpy as np
-import pandas as pd
-import os
-import shutil
-import tarfile
 import matplotlib
-from sklearn.model_selection import train_test_split
+import numpy as np
+import os
+import pandas as pd
+import pickle
 import platform
+import shutil
+from sklearn.model_selection import train_test_split
+import tarfile
+
+import config
 
 if platform.system() == 'Darwin':
 	PROJECT_PATH = '/Users/anjalikarimpil/Google Drive/Dissertation'
@@ -16,9 +19,7 @@ else:
 	PROJECT_PATH = '/users/mscdsa2018/ask2/Projects'
 # PROJECT_PATH = '/Users/anjalikarimpil/Google Drive/Dissertation'
 # PROJECT_PATH = '/users/mscdsa2018/ask2/Projects'
-INPUT_SEQ_LENGTH = 5
-OUTPUT_SEQ_LENGTH = 1
-NUM_DIMENSIONS = 2
+
 
 def get_data_folders():
 	'''
@@ -74,49 +75,49 @@ def read_files(file_count=1):
 def process_files(df_list):
 	'''
 	Given a list of data frames, parsees it by splitting datetime field to 
-	date and time,
+	date and time. Assigns traj_id to person-position combination if the records are 
+	nearby
 
 	Returns - 
 	a dataframe of all the processed data with trajectory_id, x_pos, y_pos, and
-	x_pos and y_pos lead for INPUT_SEQ_LENGTH window size. Now we have all data
+	x_pos and y_pos lead for config.INPUT_SEQ_LENGTH window size. Now we have all data
 	for training in one row
 	'''
 	processed_df_list = []
 	TIME_THRESHOLD = pd.to_timedelta('00:00:02.00000')
 	POSITION_THRESHOLD = 500
 	for df in df_list:
-		df['date'], df['time'] = df[0].str.split('T', 1).str
-		df[0] = pd.to_datetime(df[0], format="%Y-%m-%dT%H:%M:%S:%f")
-		df.columns = ['datetime', 'place', 'x_pos', 'y_pos', 'person_id', 'date', 'time']
+		df.columns = ['datetime', 'place', 'x_pos', 'y_pos', 'person_id']
+		df['date'], df['time'] = df['datetime'].str.split('T', 1).str
+		df['datetime'] = pd.to_datetime(df['datetime'], format="%Y-%m-%dT%H:%M:%S:%f")
 
-		df.sort_values(['person_id','datetime'], inplace=True, ascending=True)
+		df.sort_values(['person_id', 'datetime'], inplace=True, ascending=True)
 		df.reset_index()
 		df['time_lag'] = df.groupby(['person_id', 'date'])['datetime'].shift(1)
 		df['person_lag'] = df['person_id'].shift(1)
 
 		# flag 1 
-		df['fl_1'] = (abs(df['time_lag'] - df['datetime']) > TIME_THRESHOLD) |\
-			(df['person_lag'] != df['person_id'])
-		df['traj_id'] = df['fl_1'].cumsum()
+		df['flag_1'] = (abs(df['time_lag'] - df['datetime']) > TIME_THRESHOLD) | \
+						(df['person_lag'] != df['person_id'])
+		df['traj_id'] = df['flag_1'].cumsum()
 		df['x_lag'] = df.groupby(['traj_id'])['x_pos'].shift(1)
 		df['y_lag'] = df.groupby(['traj_id'])['y_pos'].shift(1)
 
 		df['x_diff'] = abs(df['x_pos'] - df['x_lag'])
 		df['y_diff'] = abs(df['y_pos'] - df['y_lag'])
-		df['fl_2'] = (df['x_diff'] > POSITION_THRESHOLD) | \
-			(df['y_diff'] > POSITION_THRESHOLD)
-		df['fl_3'] = (df['fl_1'] | df['fl_2'])
-		df['traj_id'] = df['fl_3'].cumsum()
+		df['flag_2'] = (df['x_diff'] > POSITION_THRESHOLD) | (df['y_diff'] > POSITION_THRESHOLD)
+		df['flag_3'] = (df['flag_1'] | df['flag_2'])
+		df['traj_id'] = df['flag_3'].cumsum()
 
-		data = df[['traj_id','x_pos','y_pos']].copy()
+		position_df = df[['traj_id', 'x_pos', 'y_pos']].copy()
 
-		for i in range(1, INPUT_SEQ_LENGTH + OUTPUT_SEQ_LENGTH):
-			data['x_' + str(i)] = data.groupby(['traj_id'])['x_pos'].shift(-i)
-			data['y_' + str(i)] = data.groupby(['traj_id'])['y_pos'].shift(-i)
+		for i in range(1, config.INPUT_SEQ_LENGTH + config.OUTPUT_SEQ_LENGTH):
+			position_df['x_' + str(i)] = position_df.groupby(['traj_id'])['x_pos'].shift(-i)
+			position_df['y_' + str(i)] = position_df.groupby(['traj_id'])['y_pos'].shift(-i)
 		# Remove NAs 
-		data = data.dropna()
-		processed_df_list.append(data)
-	return pd.concat(processed_df_list, ignore_index=True)
+		position_df = position_df.dropna()
+		processed_df_list.append(position_df)
+	return processed_df_list
 
 
 def next_batch(batch, batch_size, filt_X, filt_Y):
@@ -126,17 +127,19 @@ def next_batch(batch, batch_size, filt_X, filt_Y):
 	x_batch = []
 	y_batch = []
 	for i in range(batch_size):
-
-		x_batch.append(filt_X[batch*batch_size+i])
-		y_batch.append(filt_Y[batch*batch_size+i])
+		x_batch.append(filt_X[batch * batch_size + i])
+		y_batch.append(filt_Y[batch * batch_size + i])
 
 	return x_batch, y_batch
 
 def get_data(force_preprocess=False):
 	'''
-	Returns training, test, and dev data for input and labels that are numpy arrays.
-	Input data is of the shape num_rows x NUM_DIMENSIONS x INPUT_SEQ_LENGTH and 
-	Labels are of the shape num_rows x NUM_DIMENSIONS x OUTPUT_SEQ_LENGTH
+	Reads data from files, processes it and splits it into test, training and dev sets.
+	Reads from pre-existing file from disc if it is present.
+	Returns -
+		6 numpy arrays of training, test, and dev data for input and labels.
+		Input data is of the shape num_rows x config.NUM_DIMENSIONS x config.INPUT_SEQ_LENGTH and 
+		Labels are of the shape num_rows x config.NUM_DIMENSIONS x config.OUTPUT_SEQ_LENGTH
 	'''
 	if force_preprocess or not os.path.exists('processed_file'):
 		df_list, problem_files = read_files()
@@ -148,37 +151,130 @@ def get_data(force_preprocess=False):
 	return X_train, y_train, X_test, y_test, X_dev, y_dev
 
 
-def split_data(data):
+def split_data(data, train=0.7, test=0.2, dev=0.1):
 	'''
-	Given data as a Pd dataframe, returns 3 sets of numpy arrays (train, test and dev)
-	using train_test_split from sklearn.model_selection
+	Given data as a Pd dataframe, splits it into 3 numpy arrays (train, test and dev) 
+	for input and labels using train_test_split from sklearn.model_selection
+	Returns -
+		3 sets of numpy arrays 1 each for train, test and dev, 
+		with input/X data of shape num_rows x config.NUM_DIMENSIONS x config.INPUT_SEQ_LENGTH 
+		and lables/Y data as numpy array of shape num_rows x config.NUM_DIMENSIONS 
 	'''	
-	TRAIN = 0.7
-	TEST = 0.2
-	DEV = 0.1
-
-	X = np.array(data.iloc[:, 1:((INPUT_SEQ_LENGTH * NUM_DIMENSIONS) + 1)])
-	Y = np.array(data.iloc[:, -(OUTPUT_SEQ_LENGTH * NUM_DIMENSIONS):])
-	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=(1 - TRAIN), 
+	X = np.array(data.iloc[:, 1:((config.INPUT_SEQ_LENGTH * config.NUM_DIMENSIONS) + 1)])
+	Y = np.array(data.iloc[:, -(config.OUTPUT_SEQ_LENGTH * config.NUM_DIMENSIONS):])
+	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=(1 - train), 
 		random_state = 1)
-	X_test, X_dev, y_test, y_dev = train_test_split(X_test, y_test, test_size=(DEV/(TEST+DEV)), 
+	X_test, X_dev, y_test, y_dev = train_test_split(X_test, y_test, test_size=(dev / (test + dev)), 
 		random_state = 1)
-	return reshape(X_train, 'x'), reshape(y_train, 'y'), reshape(X_test, 'x'),\
-	reshape(y_test, 'y'), reshape(X_dev, 'x'), reshape(y_dev, 'y')
+	return data_reshape(X_train, 'x'), data_reshape(y_train, 'y'), data_reshape(X_test, 'x'),\
+	data_reshape(y_test, 'y'), data_reshape(X_dev, 'x'), data_reshape(y_dev, 'y')
 
 
-def reshape(data, type):
+def data_reshape(data, type):
 	'''
 	Takes in a numpy array and reshapes according to current requirements.
 	For input
 	Returns -
-		numpy array of shape num_rows x NUM_DIMENSIONS x INPUT_SEQ_LENGTH 
+		numpy array of shape num_rows x config.NUM_DIMENSIONS x config.INPUT_SEQ_LENGTH 
 		for input, or,
-		numpy array of shape num_rows x NUM_DIMENSIONS for output
+		numpy array of shape num_rows x config.NUM_DIMENSIONS for output
 	'''
 	if type == 'x':
-		return data.reshape(-1, NUM_DIMENSIONS, INPUT_SEQ_LENGTH)
+		return data.reshape(-1, config.NUM_DIMENSIONS, config.INPUT_SEQ_LENGTH)
 	else:
-		return data.reshape(-1, NUM_DIMENSIONS)
+		return data.reshape(-1, config.NUM_DIMENSIONS)
+
+
+def get_pedestrian_data():
+	pickled_object = 'pedestrian_data.pickle'
+	if os.path.exists(pickled_object):
+		with open(pickled_object, 'rb') as handle:
+			pedestrian_data = pickle.load(handle)
+	else:
+		pedestrian_data = PedestrianData()
+		with open(pickled_object, 'wb') as handle:
+			pickle.dump(pedestrian_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+	return pedestrian_data
+
+class PedestrianData(object):
+	'''
+	'''
+	def __init__(self):
+		self.train_df_list, _ = read_files(file_count=1)
+		self.train_df_list = process_files(self.train_df_list)
+		self.row_counts = [len(df) for df in self.train_df_list]
+		self.total_row_count = sum(self.row_counts)
+		self.split_data()
+		print ("***************** Split")
+
+	def split_data(self):
+		'''
+		Splits the train_df_list into train_df_list, test_df and dev_df.
+		Train - test - dev split is 98 - 1 - 1.
+
+		Instance variables test_df and dev_df are set here.
+		'''
+		df = self.train_df_list[-1]
+		test_sample_size = int(0.01 * self.total_row_count)
+		dev_sample_size = int(0.01 * self.total_row_count)
+		train_df, test_df = train_test_split(df, test_size=test_sample_size)
+		train_df, dev_df = train_test_split(train_df, test_size=dev_sample_size)
+		self.train_df_list[-1] = train_df
+		self.test_df = test_df
+		self.dev_df = dev_df
+		self.row_counts[-1] = len(train_df)
+		self.total_row_count = sum(self.row_counts)
+
+	def next_batch(self, batch_num, batch_size, mode='train'):
+		'''
+		'''
+		print ("{} batch {}".format(mode, batch_num))
+		def _get_df_index_and_row(index):
+			df_index = 0
+			previous_cumulative_row_count = 0
+			row = index
+			for ix, cumulative_row_count in enumerate(cumulative_row_counts):
+				if current_index < cumulative_row_count:
+					df_index = ix
+					if ix > 0:
+						previous_cumulative_row_count = cumulative_row_counts[ix - 1]
+						row = index % previous_cumulative_row_count
+					break
+			return df_index, row
+
+		current_index = batch_num * batch_size
+		next_index = (batch_num + 1) * batch_size
+		if mode == 'train':
+			current_index = current_index % self.total_row_count
+			next_index = next_index % self.total_row_count
+			cumulative_row_counts = np.array(self.row_counts).cumsum()
+			current_df_index, current_index_df_row = _get_df_index_and_row(current_index)
+			current_index_df = self.train_df_list[current_df_index]
+			next_df_index, next_index_df_row = _get_df_index_and_row(next_index)
+			next_index_df = self.train_df_list[next_df_index]
+			if current_df_index == next_df_index:
+				next_batch = current_index_df.iloc[current_index_df_row:next_index_df_row].copy()
+			else:
+				next_batch = pd.concat([current_index_df.iloc[current_index_df_row:],
+										next_index_df.iloc[:next_index_df_row]])
+		else:
+			mode_df_dict = {
+				'test': self.test_df,
+				'dev': self.dev_df
+			}
+			df = mode_df_dict[mode]
+			next_batch = df[current_index: next_index]
+		X = np.array(next_batch.iloc[:, 1:((config.INPUT_SEQ_LENGTH * config.NUM_DIMENSIONS) + 1)])
+		Y = np.array(next_batch.iloc[:, - (config.OUTPUT_SEQ_LENGTH * config.NUM_DIMENSIONS):])
+		X = X.reshape(-1, config.NUM_DIMENSIONS, config.INPUT_SEQ_LENGTH)
+		Y = Y.reshape(-1, config.NUM_DIMENSIONS, config.OUTPUT_SEQ_LENGTH)
+		return X, Y
+
+
+
+
+
+
+
 
 
--- train_test_GRU.py
+++ train_test_GRU.py
@@ -8,29 +8,40 @@ python train_test_LSTM.py
 
 Author: Mingchen Li
 '''
-import tensorflow as tf
-import numpy as np
-from tensorflow.python.ops import rnn, rnn_cell
 import matplotlib.pyplot as plt
-from data_utils import get_data
+import numpy as np
+import pickle
+import tensorflow as tf
+
+import config
 import data_utils
 
+from tensorflow.python.ops import rnn, rnn_cell
 
-num_feature = 5
-batch_size = 30
+from data_utils import get_data
+
+
+batch_size = 3
 rnn_size = 400
 num_layers = 3
 output_size = 1
 learning_rate = 0.05
 
-inputs = tf.placeholder('float', [None, 2, num_feature], name = 'inputs')
+inputs = tf.placeholder('float', [None, 2, config.INPUT_SEQ_LENGTH], name = 'inputs')
 targets = tf.placeholder('float', name = 'targets')
 
 weight = tf.Variable(tf.truncated_normal([rnn_size, 2]), name = 'weight')
 bias = tf.Variable(tf.constant(0.1, shape=[2]),name = 'bias')
 
-training_X, training_Y, dev_X, dev_Y, testing_X, testing_Y = get_data()
+# training_X, training_Y, dev_X, dev_Y, testing_X, testing_Y = get_data()
+pedestrian_data = data_utils.get_pedestrian_data()
+# pickled_object = '/Users/anjalikarimpil/Google Drive/Dissertation/Project code/' + \
+#                     'pedestrian-trajectory-predictor/ped_data.pickle'
+# with open(pickled_object, 'wb') as handle:
+#     pickle.dump(pedestrian_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+# # with open(pickled_object, 'rb') as handle:
+# #     pedestrian_data = pickle.load(handle)
 '''
 This function defines a RNN. It is an LSTM RNN for now, but if want to change to GRU, just change the
 cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_size,state_is_tuple=True)
@@ -47,13 +58,16 @@ def recurrent_neural_network(inputs, w, b):
     initial_state = cell.zero_state(batch_size=batch_size, dtype=tf.float32)
 
     outputs, last_State = tf.nn.dynamic_rnn(cell, inputs, dtype = tf.float32, scope = "dynamic_rnn")
+    print("Output shape is ", outputs.shape)
     outputs = tf.transpose(outputs, [1, 0, 2])
     # He has transposed here to facilitate gathering. Refer this
     # https://stackoverflow.com/questions/36764791/in-tensorflow-how-to-use-tf-gather-for-the-last-dimension
     # This is beacuse tensorflow doesnt let you to do just do array[,:-1]
     last_output = tf.gather(outputs, 1, name="last_output")
-    
+    print("Last Output shape is ", last_output.shape)
     prediction = tf.matmul(last_output, w) + b
+    print("Prediction shape is ", prediction.shape)
+    prediction = tf.reshape(prediction, (-1, prediction.shape[1], config.OUTPUT_SEQ_LENGTH))
     return prediction
 
 '''
@@ -65,8 +79,10 @@ evaluate the model performance. The file has two rows, with the first row being 
 def train_neural_network(inputs):
     
     prediction = recurrent_neural_network(inputs, weight, bias)
-    #print(prediction.shape)
-    #print(tf.reduce_sum(prediction - targets, 0).shape)
+    print('prediction', prediction.shape)
+    print('target shape', targets.shape)
+    print('shape of result of tf.reduce_sum(prediction - targets, 0)', tf.reduce_sum(prediction - targets, 0).shape)
+
     cost = tf.reduce_sum(tf.square(tf.norm(prediction - targets, ord='euclidean', axis=1)))
     # The cost looks like it's the mean squared error, ie. sum of squared errors
     #cost = tf.square(tf.norm(tf.reduce_sum(prediction - targets, 0)))      # prediction: (len,2)
@@ -82,43 +98,28 @@ def train_neural_network(inputs):
         while (abs(train_epoch_loss - prev_train_loss) > 1e-5):
             iteration += 1
             prev_train_loss = train_epoch_loss
-            #train_epoch_loss = 0
-            
-            # for batch in range(int(len(training_X)/batch_size)):            # There will be some data that's been thrown away if the size of
-            #                                                                 # training_X is not divisible by batch_size
-            #     x_batch, y_batch = read_data.next_batch(batch, batch_size, training_X, training_Y)
-            #     data_feed = {inputs: x_batch, targets: y_batch}
-            #     _, c = sess.run([optimizer, cost], data_feed)
-                #print('train: ', c)
-                #train_epoch_loss += c/batch_size
-            '''
-            # training cost
-            data_feed = {inputs: training_X, targets: training_Y}
-            _, train_c = sess.run([optimizer, cost], data_feed)
-            train_epoch_loss = train_c/len(training_X)
-            '''
-            #train_epoch_loss = train_epoch_loss/int(len(training_X)/batch_size)        # Use the same expression as above to make
-                                                                                    # sure not count the data that is thrown away
-            
-
 
             dev_epoch_loss = 0
-            for batch in range(int(len(dev_X)/batch_size)):
-                x_batch, y_batch = data_utils.next_batch(batch, batch_size, dev_X, dev_Y)
+            for batch in range(int(len(pedestrian_data.dev_df)/batch_size)):
+                # x_batch, y_batch = data_utils.next_batch(batch, batch_size, dev_X, dev_Y)
+                x_batch, y_batch = pedestrian_data.next_batch(mode='dev', batch_num=batch,
+                                                              batch_size=batch_size)
                 data_feed = {inputs: x_batch, targets: y_batch}
                 c = sess.run(cost, data_feed)
                 #print('dev: ', c)
                 dev_epoch_loss += c/batch_size
-            dev_epoch_loss = dev_epoch_loss / (int(len(dev_X) / batch_size))
+            dev_epoch_loss = dev_epoch_loss / (int(len(pedestrian_data.dev_df) / batch_size))
             # training cost
             train_epoch_loss = 0
-            for batch in range(int(len(training_X)/batch_size)):
-                x_batch, y_batch = data_utils.next_batch(batch, batch_size, training_X, training_Y)
+            for batch in range(int(pedestrian_data.total_row_count/batch_size)):
+                # x_batch, y_batch = data_utils.next_batch(batch, batch_size, training_X, training_Y)
+                x_batch, y_batch = pedestrian_data.next_batch(mode='train', batch_num=batch,
+                                                              batch_size=batch_size)
                 data_feed = {inputs: x_batch, targets: y_batch}
                 _, c = sess.run([optimizer, cost], data_feed)
                 #print('dev: ', c)
                 train_epoch_loss += c / batch_size
-            train_epoch_loss = train_epoch_loss / (int(len(training_X) / batch_size))
+            train_epoch_loss = train_epoch_loss / (int(pedestrian_data.total_row_count / batch_size))
             # denominator int(len(training_X)/batch_size) is number of batches
             # train_epoch_loss outside the loop gives per instance loss across all batches
 
@@ -131,11 +132,11 @@ def train_neural_network(inputs):
             '''
             train_cost_list.append(train_epoch_loss)
             dev_cost_list.append(dev_epoch_loss)
-            print('Train iteration', iteration,'train loss:',train_epoch_loss)
-            print('Train iteration', iteration,'dev loss:',dev_epoch_loss)
-            if iteration == 15:
+            print('Train iteration', iteration,'train loss:', train_epoch_loss)
+            print('Train iteration', iteration,'dev loss:', dev_epoch_loss)
+            if iteration == 2:
                 break
-        iter_list = range(1, iteration+1)
+        iter_list = range(1, iteration + 1)
         plt.figure(1)
         plt.plot(iter_list, train_cost_list)
         plt.plot(iter_list, dev_cost_list)
@@ -149,29 +150,40 @@ def train_neural_network(inputs):
 
         # Begin testing
         test_epoch_loss = 0
-        test_prediction = np.empty([int(len(testing_X)), 2])
+        test_prediction = np.empty([len(pedestrian_data.test_df), 2, config.OUTPUT_SEQ_LENGTH])
         '''
         data_feed = {inputs: testing_X, targets: testing_Y}
         pre, test_c = sess.run([prediction, cost], data_feed)
         test_prediction = pre
         test_epoch_loss = test_c/int(len(testing_X))
         '''
-        test_prediction = np.empty([int(len(testing_X)/batch_size)*batch_size, 2])
-        for batch in range(int(len(testing_X)/batch_size)):
-            x_batch, y_batch = data_utils.next_batch(batch, batch_size, testing_X, testing_Y)
+        test_prediction = np.empty([int(len(pedestrian_data.test_df) / batch_size) * batch_size, 2, config.OUTPUT_SEQ_LENGTH])
+        y_batch_list = []
+        x_batch_list = []
+        for batch in range(int(len(pedestrian_data.test_df) / batch_size)):
+            # x_batch, y_batch = data_utils.next_batch(batch, batch_size, testing_X, testing_Y)
+            x_batch, y_batch = pedestrian_data.next_batch(mode='test', batch_num=batch,
+                                                          batch_size=batch_size)
+            y_batch_list.append(y_batch)
+            x_batch_list.append(x_batch)
             data_feed = {inputs: x_batch, targets: y_batch}
             pre, c = sess.run([prediction, cost], data_feed)
             pre = np.array(pre)
             test_epoch_loss += c
+            import ipdb; ipdb.set_trace()
             test_prediction[batch*batch_size : (batch+1)*batch_size, :] = pre
-        test_epoch_loss = test_epoch_loss/(int(len(testing_X)/batch_size)*batch_size)
+        testing_Y = np.concatenate(y_batch_list, axis=0)
+        testing_X = np.concatenate(x_batch_list, axis=0)
+        test_epoch_loss = test_epoch_loss/(int(len(pedestrian_data.test_df)/batch_size)*batch_size)
         
-        print('Test loss:',test_epoch_loss)
+        print('Test loss:', test_epoch_loss)
 
         # Save predicted data and ground truth data into a .csv file.
         test_prediction = np.transpose(test_prediction)                                                             # The first row of file: prediction
         testing_Y_array = np.transpose(np.array(testing_Y)[0 : int(len(testing_X)/batch_size)*batch_size, :])       # The second row of file: ground truth
         test_prediction_and_real = np.vstack((test_prediction, testing_Y_array))
+        test_prediction_and_real = test_prediction_and_real.reshape(
+                    (test_prediction_and_real.shape[0] * test_prediction_and_real.shape[1], -1))
         np.savetxt("GRU_test_prediction_and_real.csv", test_prediction_and_real, delimiter = ",")
 
 
